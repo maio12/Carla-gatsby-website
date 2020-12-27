03bde2d5fecd2f8d2dce774bab9022a62164ba75
Antonio_Maiocchi <amaiocchi@red.software.systems>
2020-12-27 22:11:16 +0100

    cleanup homepage links

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const IndexPage = () => {
         <h1>Hello</h1>
         <h2>I'm Carla, living in Madrid</h2>
         <p>Need to go outside the site? <a href='https://facebook.com' target='_/blank'>Facebook</a></p>
-        <p>Need a therapist? <Link to='/contact'>Contact me</Link></p>
       </Layout>
     </div>
   )
